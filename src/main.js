f3f8aac697d2850177de94b170d65b0c9d2b4204
Ebenezer Nikabou <nikaboue10@gmail.com>
2023-12-20 23:24:04 +0200

    added basic interactive mode logic

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 import "dotenv/config";
 import fs from "fs";
+import readline from "readline";
 import { sendOpenApiRequest } from "./request-helper.js";
 import { logger } from "./utilities/logger.js";
+import { appGlobalConsts } from "./consts/app-global-consts.js";
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let askFunctionCalledTimes = 0;
 export const ask = (prompt) => {
+  if (askFunctionCalledTimes === 0) {
+    logger.logPrompt(prompt + '\n');
+  }
+  askFunctionCalledTimes++;
   sendOpenApiRequest(prompt)
     .then((data) => {
       logger.logResponse(data.choices[0].message.content);
+      rl.question(`${appGlobalConsts.colorizedUserPromptPrefix} : `, (newPrompt) => {
+        ask(newPrompt);
+      });
     })
     .catch((error) => {
       logger.logError(JSON.parse(error.message));
